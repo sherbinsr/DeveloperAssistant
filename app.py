6028fd936053f05c333f88140856983c28020f37
sherbinsr <sherbinsyles31@gmail.com>
2024-08-12 22:14:38 +0530

    chat history

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from langchain.prompts import  PromptTemplate
 from langchain.llms import  CTransformers
 from langchain.memory import ConversationBufferMemory
 
+### Dictonary to collect chat history
+chat_history = []
+
 ## Function to get response from LLAMA 2 Model
 def getResponse(input_text, language):
     ### LLama2 model
@@ -14,7 +17,7 @@ def getResponse(input_text, language):
     ### Prompt Template
 
     template="""
-      Write the code using {language} for {input_text}
+      Write the code for {input_text} using {language}
     """
     prompt = PromptTemplate(input_variables=["language", "input_text"],
                             template=template
@@ -23,6 +26,13 @@ def getResponse(input_text, language):
     memory = ConversationBufferMemory()
     ## Generate response
     response = llm(prompt.format(language=language , input_text= input_text))
+    chat_history.append(
+        {
+            "question": input_text,
+            "language": language,
+            "response": response
+
+        })
     memory.save_context(
         {"input": input_text},
         {"output": response})
@@ -64,3 +74,19 @@ submit = st.button("Generate")
 
 if submit:
     st.write(getResponse(input_text,language))
+
+# Layout with columns to position the clear button on the right side
+col1, col2 = st.columns([9, 1])  
+
+with col1:
+    if chat_history:
+        with st.expander("### Chat History", expanded=False):
+            for chat in chat_history:
+                st.write(f"**You:** {chat['question']}")
+                st.write(f"**Assistant:** {chat['response']}")
+                st.write("---")
+
+with col2:
+    if st.button("Clear History"):
+        chat_history.clear()
+        st.experimental_rerun()
